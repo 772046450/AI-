6af550793b99dd9414ad7979b21c95161425e7fb
KKKuko <93979706+772046450@users.noreply.github.com>
2024-04-04 23:35:12 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,39 +1,76 @@
-package com.kuko.baseapp;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.kuko.project4_2;
 
 import android.os.Bundle;
-
-
-
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    Button button1;
+public class MainActivity extends AppCompatActivity {
+    TextView text1, text2;
+    CheckBox chkAgree;
+    RadioGroup rGroup1;
+    RadioButton rdoDog, rdoCat, rdoRabbit;
+    Button btnOK;
+    ImageView imgPet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("애완동물 사진 보기");
+        text1 =(TextView) findViewById(R.id.Text1);
+        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
 
-        button1=(Button) findViewById(R.id.button1);
-        button2=(Button) findViewById(R.id.button2);
+        text2 =(TextView) findViewById(R.id.Text2);
+        rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
+        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
+        rdoCat = (RadioButton)findViewById(R.id.RdoCat);
+        rdoRabbit = (RadioButton)findViewById(R.id.RdoRabbit);
+        btnOK =(Button) findViewById(R.id.BtnOK);
+        imgPet = (ImageView) findViewById(R.id.ImgPet);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        // 为复选框设置状态改变监听器
+        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-
-                Toast.makeText(getApplicationContext(),"버튼을 늘렸어요",
-                            Toast.LENGTH_SHORT).show();
-
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (chkAgree.isChecked()==true) {
+                    // 显示相关视图
+                    text2.setVisibility(android.view.View.VISIBLE);
+                    rGroup1.setVisibility(android.view.View.VISIBLE);
+                    btnOK.setVisibility(android.view.View.VISIBLE);
+                    imgPet.setVisibility(android.view.View.VISIBLE);
+                } else {
+                    // 隐藏相关视图
+                    text2.setVisibility(android.view.View.INVISIBLE);
+                    rGroup1.setVisibility(android.view.View.INVISIBLE);
+                    btnOK.setVisibility(android.view.View.INVISIBLE);
+                    imgPet.setVisibility(android.view.View.INVISIBLE);
+                }
             }
         });
-        button2.setOnClickListener(new View.OnClickListener() {
+
+        // 为确定按钮设置点击监听器
+        btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Button 2",
-                        Toast.LENGTH_SHORT).show();
+            public void onClick(View arg0) {
+                int checkedRadio = rGroup1.getCheckedRadioButtonId();
+                if (checkedRadio == R.id.RdoDog)
+                    imgPet.setImageResource(R.drawable.dog);
+                else if (checkedRadio == R.id.RdoCat)
+                    imgPet.setImageResource(R.drawable.cat);
+                else if (checkedRadio == R.id.RdoRabbit)
+                    imgPet.setImageResource(R.drawable.rabbit);
+                else
+                    if (checkedRadio == -1)
+                    Toast.makeText(getApplicationContext(),"동물 먼저 선택하세요", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
